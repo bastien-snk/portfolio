9da9ca0246892282763fd6d41e6d869990797d61
rootxls <45496261+rootxls@users.noreply.github.com>
2021-10-26 21:57:58 +0200

    :construction: Add competences component

---
+++ src/pages/Competences.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header.js'
+
+function Competences() {
+    return (
+        <div className="bg-theme-gray-200 min-w-screen min-h-screen">
+            <Header />
+        </div>
+    );
+}
+
+export default Competences;

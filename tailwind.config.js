825f57d95721836166118f58422a849887b90b3a
rootxls <45496261+rootxls@users.noreply.github.com>
2021-10-27 18:59:49 +0200

    :wrench: Add more border widths to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,46 +9,59 @@ module.exports = {
       fontFamily: {
         'montserrat': ['Montserrat', 'sans-serif'],
         'fira-code': ['"Fira Code"']
-      }
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      indigo: colors.indigo,
-      red: colors.red,
-      yellow: colors.yellow,
-      orange: colors.orange,
-      green: colors.green,
-      theme: {
-        white: {
-          classic: '#F8F8F8',
-          glass: '#E4E4E4',
-        },
-        yellow: {
-          F7B23B: '#F7B23B',
-          F6AA28: '#F6AA28',
-          F49F0A: '#F49F0A',
-        },
-        gray: {
-          250: '#292929',
-          200: '#303030',
-          150: '#3D3D3D',
-          100: '#474747',
-          50: '#7A7A7A',
-        },
-        dev: {
-          orange: '#F6AA28',
-          white: '#F8F8F8',
-          yellow: '#FBD99D',
-          green: '#7DCE82',
-          blue: '#6A98F0',
-          gray: '#C4C4C4',
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '1': '1px',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '5': '5px',
+        '6': '6px',
+        '7': '7px',
+        '8': '8px',
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        indigo: colors.indigo,
+        red: colors.red,
+        yellow: colors.yellow,
+        orange: colors.orange,
+        green: colors.green,
+        theme: {
+          white: {
+            classic: '#F8F8F8',
+            glass: '#E4E4E4',
+          },
+          yellow: {
+            F7B23B: '#F7B23B',
+            F6AA28: '#F6AA28',
+            F49F0A: '#F49F0A',
+          },
+          gray: {
+            250: '#292929',
+            200: '#303030',
+            150: '#3D3D3D',
+            100: '#474747',
+            50: '#7A7A7A',
+          },
+          dev: {
+            orange: '#F6AA28',
+            white: '#F8F8F8',
+            yellow: '#FBD99D',
+            green: '#7DCE82',
+            blue: '#6A98F0',
+            gray: '#C4C4C4',
+          }
         }
       }
-    }
+    },
+
   },
   variants: {
     extend: {},

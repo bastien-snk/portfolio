df774fbee587adfea456cd96711f0df96ae6d905
rootxls <45496261+rootxls@users.noreply.github.com>
2021-10-28 14:37:30 +0200

    :bug: Fix netlify href build error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const hoverAnimation = "transform hover:scale-125 duration-150 hover:-rotate-6";
 class Header extends Component {
     render() {
         return <div className={"flex font-montserrat pt-7 xl:justify-center sticky top-0 z-50 shadow-inner shadow-2xl"}>
-            <a id="logo" href="" className={hoverAnimation}>
+            <a id="logo" href="#root" className={hoverAnimation}>
                 <div className="flex ml-16 xl:ml-0">
                     <img
                         className=""
@@ -23,16 +23,16 @@ class Header extends Component {
 
             <nav className="ml-12 relative">
                 <ul className="mt-2 text-theme-white-classic hidden xl:flex lg:sh">
-                    <li className={"mr-11 " + hoverAnimation }><a href={""}>📝 À Propos</a></li>
-                    <li className={"mr-11 " + hoverAnimation }><a href={""}>🔧 Compétences</a></li>
-                    <li className={"mr-11 " + hoverAnimation }><a href={""}>💼 Expérience</a></li>
-                    <li className={"mr-11 " + hoverAnimation }><a href={""}>⚙️ Mes services</a></li>
-                    <li className={"mr-11 " + hoverAnimation }><a href={""}>🖥️ Portfolio</a></li>
-                    <li className={"mr-11 " + hoverAnimation }><a href={""}>✉️ Contact</a></li>
+                    <li className={"mr-11 " + hoverAnimation }><a href={"#"}>📝 À Propos</a></li>
+                    <li className={"mr-11 " + hoverAnimation }><a href={"#"}>🔧 Compétences</a></li>
+                    <li className={"mr-11 " + hoverAnimation }><a href={"#"}>💼 Expérience</a></li>
+                    <li className={"mr-11 " + hoverAnimation }><a href={"#"}>⚙️ Mes services</a></li>
+                    <li className={"mr-11 " + hoverAnimation }><a href={"#"}>🖥️ Portfolio</a></li>
+                    <li className={"mr-11 " + hoverAnimation }><a href={"#"}>✉️ Contact</a></li>
                 </ul>
             </nav>
 
-            <a href="" className={"z-50"}>
+            <a href="#root" className={"z-50"}>
                 <img
                     className={"flex xl:hidden absolute right-7 top-7 " + hoverAnimation }
                     src={ menu }

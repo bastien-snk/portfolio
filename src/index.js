c7d0ad498ebd7ea12a7381efef335d4b9bd9bf55
rootxls <45496261+rootxls@users.noreply.github.com>
2021-11-02 14:23:44 +0100

    :truck: Add route system

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
-
+import {Route, BrowserRouter as Router} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Competences from "./pages/Competences";
+import {Experience} from "./pages/experience/Experience";
+import {Portfolio} from "./pages/portfolio/Portfolio";
+import {Services} from "./pages/services/Services";
+import {Contact} from "./pages/contact/Contact";
 
 ReactDOM.render(
   <React.StrictMode>
-      <Home />
+
+      <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={Home}  />
+          <Route path="/competences" exact component={Competences} />
+          <Route path="/experience" exact component={Experience} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/portfolio" exact component={Portfolio} />
+          <Route path="/contact" exact component={Contact} />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

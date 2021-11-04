6b2c1d8b68a5a7f1510e64b3d88f122c4d16c516
rootxls <45496261+rootxls@users.noreply.github.com>
2021-11-04 16:22:09 +0100

    :lipstick: Add Button Component themes

--- src/components/design-system/Button.jsx
+++ src/components/design-system/Button.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 
+const theme = {
+    bold: {
+        bg: " bg-theme-yellow-F49F0A ",
+        text: " text-theme-white-classic ",
+        border: "",
+    },
+    light: {
+        bg: " bg-transparent ",
+        text: " text-theme-yellow-F49F0A ",
+        border: " border-4 border-theme-yellow-F49F0A "
+    }
+}
+
+function getTheme(selected) {
+    switch (selected) {
+        case "light":
+            return theme.light;
+
+        default:
+            return theme.bold;
+    }
+}
+
 class Button extends React.Component {
     render(props) {
+        let selectedTheme = getTheme(this.props.mode);
+
         return <a href={ "" + this.props.href }>
-            <div type="button" className="flex bg-theme-yellow-F49F0A rounded-full w-48 h-14 my-10 transform hover:scale-125 duration-150 hover:-rotate-6">
-                <span className="flex-1 uppercase text-xl text-center my-auto">{ this.props.text }</span>
+            <div type="button" className={"flex rounded-full h-14 my-10 transform hover:scale-125 duration-150 hover:-rotate-6 " + selectedTheme.bg + selectedTheme.border }>
+                <span className={"flex-1 uppercase text-xl text-center my-auto px-5" + selectedTheme.text }>{ this.props.text }</span>
             </div>
         </a>;
     }

ebbc613462325f62772f5211e188b35e1bd5a1df
rootxls <45496261+rootxls@users.noreply.github.com>
2021-11-05 16:46:58 +0100

    :bug: Fix footer images not displaying bug

--- src/components/parts/Footer.jsx
+++ src/components/parts/Footer.jsx
@@ -11,23 +11,23 @@ export const mode = {
     }
 }
 
-export function Footer({selectedMode, imgPath}) {
+export function Footer({selectedMode}) {
 
     return (<div className={selectedMode.background + " text-theme-white-classic font-montserrat py-14"}>
             <hr className="mx-auto border-theme-white-classic bg-theme-white-classic border-1 w-80 rounded-full" />
-            <h1 className="text-xl text-center mt-4">© 2021 - Développé par <a href={social.GITHUB} className="text-theme-yellow-F49F0A">moi-même</a></h1>
-            <div>
+            <h1 className="text-xl text-center mt-4 ">© 2021 - Développé par <a href={social.GITHUB} className="text-theme-yellow-F49F0A ">moi-même</a></h1>
+            <div className="py-3 flex justify-center gap-x-4">
                 {
                     Object.values(social).map(network =>
                         <a key={network.img} href={"" + network.link}>
                             <img
-                                className={"" /*+ hoverAnimation*/ }
-                                src={ imgPath + "social/" + network.img }
-                                srcSet={ imgPath + "social/" + network.img }
+                                className={"transform hover:skew-y-12 transition duration-300"}
+                                src={ network.img }
+                                srcSet={ network.img }
                                 alt=""
                                 aria-valuetext="test"
-                                width="80"
-                                height="80"
+                                width="30"
+                                height="30"
                             />
                         </a>
 

4d81693b39a3f6e1abd9f567acda4481cffc67bc
rootxls <45496261+rootxls@users.noreply.github.com>
2021-11-05 19:03:41 +0100

    :dizzy: Add footer transition to Portfolio page

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -8,6 +8,8 @@ import sonic from "../../img/projects/sonic.png";
 import arkane from "../../img/projects/arkane.png";
 import ludi from "../../img/projects/ludi.png";
 import bolt from "../../img/projects/bolt.png";
+import transition from "../../img/transitions/portfolio/1.png";
+import {Footer, mode} from "../../components/parts/Footer";
 
 const projectTypes = {
     PERSONNAL: {
@@ -132,13 +134,23 @@ export class Portfolio extends React.Component {
                 <Header />
                 <Section name="🖥️ Portfolio" id="portfolio" />
 
-                <div className="py-28 justify-self-center grid lg:grid-cols-2 xl:grid-cols-3 gap-x-12 gap-y-10 mx-auto w-10/12">
+                <div className="pt-28 justify-self-center grid lg:grid-cols-2 xl:grid-cols-3 gap-x-12 gap-y-10 mx-auto w-10/12">
                     {projects.map(project =>
                         <div key={projects.indexOf(project)} className="justify-self-center">
                             <ProjectCard project={project} />
                         </div>)
                     }
                 </div>
+
+                <img
+                    className="bottom-0 z-40 mt-32 bg-theme-gray-250"
+                    src={transition}
+                    srcSet={transition}
+                    width="100%"
+                    height="100%"
+                    alt=""
+                />
+                <Footer selectedMode={mode.DARK} />
             </div>
         );
     }

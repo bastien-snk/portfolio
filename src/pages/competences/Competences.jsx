1e611972f4fa7b71a652eb9f64e80875a7686404
RootXLS <45496261+rootxls@users.noreply.github.com>
2021-11-09 16:37:32 +0100

    :construction: Add section competence structure

--- src/pages/competences/Competences.jsx
+++ src/pages/competences/Competences.jsx
@@ -1,16 +1,24 @@
 import Header from '../../components/parts/Header.jsx';
 import { Doughnut, Pie, ChartProps } from 'react-chartjs-2';
+import {Footer, mode} from "../../components/parts/Footer";
+import Section from "../../components/design-system/Section";
 
 function Competences() {
     return (
         <div className="bg-theme-gray-200 min-w-screen min-h-screen">
             <Header />
-            <div className="w-2/12 h-2/12">
+            <Section name="🔧 Compétences" id="competences" />
+
+            <div>
+
+            </div>
+
+            {/*<div className="w-2/12 h-2/12 text-theme-white-classic">
                 <Doughnut
                     options={{
                         plugins: {
                             title: {
-                                display: true,
+                                display: false,
                                 text: 'Custom Chart Title'
                             }
                         }
@@ -19,7 +27,7 @@ function Competences() {
                         labels: ["React Native", "Android"],
                         datasets: [
                             {
-                                data: [10,90],
+                                data: [50,50],
                                 backgroundColor: ['#D18A0C', '#F6AA28'],
                                 borderWidth: 0,
                             },
@@ -28,8 +36,9 @@ function Competences() {
 
 
                 />
-            </div>
+            </div>*/}
 
+            <Footer selectedMode={mode.DARK} />
         </div>
     );
 }

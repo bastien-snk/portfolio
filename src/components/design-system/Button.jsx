cd225032ed337a8d4221b5c2c552d75411827b11
RootXLS <45496261+rootxls@users.noreply.github.com>
2021-11-15 15:22:02 +0100

    :iphone: Work on Button component responsive design

--- src/components/design-system/Button.jsx
+++ src/components/design-system/Button.jsx
@@ -28,8 +28,8 @@ class Button extends React.Component {
         let selectedTheme = getTheme(this.props.mode);
 
         return <a href={ "" + this.props.href }>
-            <div type="button" className={"flex rounded-full h-14 my-10 transform hover:scale-125 duration-150 hover:-rotate-6 " + selectedTheme.bg + selectedTheme.border }>
-                <span className={"flex-1 uppercase text-xl text-center my-auto px-5" + selectedTheme.text }>{ this.props.text }</span>
+            <div type="button" className={"flex rounded-full py-2 xl:py-4 w-auto transform hover:scale-125 duration-150 hover:-rotate-6" + selectedTheme.bg + selectedTheme.border }>
+                <span className={"flex-1 text-center my-auto text-white uppercase text-2xl px-5" + selectedTheme.text }>{ this.props.text }</span>
             </div>
         </a>;
     }
